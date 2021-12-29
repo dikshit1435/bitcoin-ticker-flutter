b4f737116b1583bff404ef0e90726a606e36b0b1
dikshit1435 <dikshit.d.k.2002@gmail,com>
2021-12-29 16:00:56 +0530

    Upgrage Api

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:http/http.dart'as http;
+import 'dart:convert';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,29 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String CurrencyValue = 'USD';
+  String bitcoin  = 'BTC';
+  int rate;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+  Future getData()async {
+   http.Response response = await http.get(Uri.parse('https://rest.coinapi.io/v1/exchangerate/$bitcoin/$CurrencyValue?apikey=C06C60A5-2405-4971-BA89-0841770C6A39'));
+   var data = response.body;
+  var decodedData= jsonDecode(data);
+   double tempRate =  decodedData['rate'];
+   setState(() {
+     rate =  tempRate.toInt();
+   });
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +42,11 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
+              margin: EdgeInsets.only(bottom: 400),
               color: Colors.lightBlueAccent,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
@@ -27,7 +55,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 $bitcoin =  $rate $CurrencyValue',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -37,13 +65,65 @@ class _PriceScreenState extends State<PriceScreen> {
               ),
             ),
           ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+
+
+
+          Row(
+              
+            children: [
+
+              Flexible(
+                child: Container(
+                  height: 50.0,
+                  alignment: Alignment.center,
+
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  color: Colors.cyan,
+                  child: DropdownButton<String>(
+                    value: CurrencyValue,
+                    elevation: 16,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        CurrencyValue = newValue;
+                        getData();
+                      });
+                    },
+                    items: currenciesList.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  )
+                ),
+              ),
+              Flexible(
+                child: Container(
+                    height: 50.0,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    color: Colors.blueAccent,
+                    child: DropdownButton<String>(
+                      value: bitcoin,
+                      elevation: 16,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          bitcoin = newValue;
+                          getData();
+                        });
+                      },
+                      items: cryptoList.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    )
+                ),
+              ),
+            ],
           ),
+
         ],
       ),
     );
